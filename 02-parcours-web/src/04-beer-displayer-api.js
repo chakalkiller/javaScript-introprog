fed7c791d48c74c15672af8ef1a3d4049814b92b
chakalkiller <umina.tony@gmail.com>
2022-03-23 06:15:14 +0100

    test

--- 02-parcours-web/src/04-beer-displayer-api.js
+++ 02-parcours-web/src/04-beer-displayer-api.js
@@ -1,16 +1,46 @@
-async function getData() {
-    try {
-      const response = await fetch(
-        "$ curl https://api.punkapi.com/v2/beers?page=2&per_page=80 "
-      );
-      // await: attend la fin de l'opération puis passe à la suite
-      // sans bloquer l’exécution des autres opérations asynchrones
-      // qui pourraient se dérouller en arrière plan
-      const data = await response.json();
-      const {beer_name, tagline, first_brewed,image_url} = data;
-      console.log(beer_name, tagline, first_brewed,image_url);
-    } catch (error) {
-      console.log("Erreur : " + error);
-    }
+
+let b1 = document.querySelector("#inputOne");
+
+
+function plus() {
+  let id = document.querySelector("#id").value;
+  
+  if (id) {
+      document.querySelector("#result").value = parseFloat(inputOne) + parseFloat(inputTwo);
   }
-  getData();
+}
+async function getData() {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    // await: attend la fin de l'opération puis passe à la suite
+    // sans bloquer l’exécution des autres opérations asynchrones
+    // qui pourraient se dérouller en arrière plan
+    const data = await response.json();
+    const { userId, id, title, completed } = data;
+    console.log(userId, id, title, completed);
+  } catch (error) {
+    console.log("Erreur : " + error);
+  }
+}
+getData();
+
+
+// async function getData() {
+//     try {
+//       const response = await fetch("https://api.punkapi.com/v2/beers1");
+//       // await: attend la fin de l'opération puis passe à la suite
+//       // sans bloquer l’exécution des autres opérations asynchrones
+//       // qui pourraient se dérouller en arrière plan
+//       const data = await response.json();
+//       const {beer_name, tagline, first_brewed,image_url} = data;
+//       console.log(beer_name, tagline, first_brewed,image_url);
+//     } catch (error) {
+//       console.log("Erreur : " + error);
+//     }
+
+//   }
+//   getData();
+  
+b1.addEventListener("click", getData);
